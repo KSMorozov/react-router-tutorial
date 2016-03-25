1ac99c0d905ae904217c55caefbd3ac08c77159f
Kirill Morozov <tiguhrrr@gmail.com>
2016-03-25 11:47:30 +0300

    Add lesson 3 code.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router';
 
-export default () => <div>Hello React-Router</div>;
+export default () =>
+  <div>
+    <h1>React Router Tutorial</h1>
+    <ul role="nav">
+      <li><Link to="/about">About</Link></li>
+      <li><Link to="/repos">Repos</Link></li>
+    </ul>
+  </div>;
