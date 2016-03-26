8e4d705beb599a1cfe3250d69c55291cdc8cbd2a
Kirill Morozov <tiguhrrr@gmail.com>
2016-03-26 21:29:59 +0300

    Add chapter 9 code

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default ({ children }) =>
   <div>
     <h1>React Router Tutorial</h1>
     <ul role="nav">
-      <li><NavLink to="/">Home</NavLink></li>
+      <li><NavLink to="/" onlyActiveOnIndex>Home</NavLink></li>
       <li><NavLink to="/about">About</NavLink></li>
       <li><NavLink to="/repos">Repos</NavLink></li>
     </ul>
