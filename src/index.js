46d365fa0c680ddcc16539f77a1ba11101c8dd16
Kirill Morozov <tiguhrrr@gmail.com>
2016-03-26 11:31:38 +0300

    Add chapter 8 code.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import App from './App.js';
+import Home from './components/Home';
 import About from './components/About';
 import Repos from './components/Repos';
 import Repo from './components/Repo';
@@ -10,6 +11,7 @@ import './main.css';
 ReactDOM.render((
   <Router history={ hashHistory }>
     <Route path="/" component={App}>
+      <IndexRoute component={Home} />
       <Route path="/about" component={About} />
       <Route path="/repos" component={Repos}>
         <Route path="/repos/:userName/:repoName" component={Repo} />
