9824b76f2b1ee8e6bed0f37d9c11a55e070e7d53
Kirill Morozov <tiguhrrr@gmail.com>
2016-03-26 21:52:32 +0300

    Add chapter 10 code.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import App from './App.js';
 import Home from './components/Home';
 import About from './components/About';
@@ -9,7 +9,7 @@ import Repo from './components/Repo';
 import './main.css';
 
 ReactDOM.render((
-  <Router history={ hashHistory }>
+  <Router history={ browserHistory }>
     <Route path="/" component={App}>
       <IndexRoute component={Home} />
       <Route path="/about" component={About} />
