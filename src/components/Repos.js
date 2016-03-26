6c834875c70abd9a11822272c10a938b2dc38fb5
Kirill Morozov <tiguhrrr@gmail.com>
2016-03-26 09:14:26 +0300

    Add chapter 7 code.

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-export default () =>
+export default ({ children }) =>
   <div>
     <h2>Repos</h2>
     <ul>
       <li><Link to="/repos/rackt/react-router">React Router</Link></li>
       <li><Link to="/repos/facebook/react">React</Link></li>
+      {children}
     </ul>
   </div>;
